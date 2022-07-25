d59d176790e4966d387def3b7024823c31086d2a
Raphaël <lemaitre.raphael@icloud.com>
2022-07-25 12:11:48 +0200

    main.js

--- assets/main.js
+++ assets/main.js
@@ -8163,12 +8163,12 @@
                     }
 
                     function createBadge() {
-                        var $brand = $('<a class="w-webflow-badge"></a>').attr("href", "https://webflow.com?utm_campaign=brandjs");
-                        var $logoArt = $("<img>").attr("src", "https://d3e54v103j8qbb.cloudfront.net/img/webflow-badge-icon.f67cd735e3.svg").attr("alt", "").css({
-                            marginRight: "8px",
-                            width: "16px",
+                        var $brand = $('').attr("");
+                        var $logoArt = $("").attr("", "").attr("", "").css({
+                            marginRight: "",
+                            width: "",
                         });
-                        var $logoText = $("<img>").attr("src", "https://d1otoma47x30pg.cloudfront.net/img/webflow-badge-text.6faa6a38cd.svg").attr("alt", "Made in Webflow");
+                        var $logoText = $("").attr("src", "").attr("", "");
                         $brand.append($logoArt, $logoText);
                         return $brand[0];
                     }
